17dbdf22d1830e27543bb8059078b5c474582e84
Md Mosfequr <mosfequr11@users.noreply.github.com>
2026-05-04 01:12:06 +0600

    add: nav toggle in Mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
-
+const navToggle = document.querySelector(".nav-toggle");
 const sideBar = document.querySelector(".sidebar");
 
 // Add "show-sidebar" class from Sidebar
 navToggle.addEventListener("click", function () {
   sideBar.classList.toggle("show-sidebar");
+});
+
+const closeBtn = document.querySelector(".close-btn");
+// Remove "show-sidebar" class from Sidebar
+closeBtn.addEventListener("click", function () {
+  sideBar.classList.remove("show-sidebar");
 });
