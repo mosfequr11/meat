8ecce8f7d9759ff042212e143f74756df533a5ac
Md Mosfequr <mosfequr11@users.noreply.github.com>
2026-05-03 21:34:02 +0600

    add: Mobile menu

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+const sideBar = document.querySelector(".sidebar");
+
+// Add "show-sidebar" class from Sidebar
+navToggle.addEventListener("click", function () {
+  sideBar.classList.toggle("show-sidebar");
+});
